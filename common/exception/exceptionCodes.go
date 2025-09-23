26aba5eba03a54b3f93b95d8c504087348950052
tonyp8 <tonyp8@126.com>
2025-09-23 23:26:13 +0800

    chore: 新增错误类型

--- common/exception/exceptionCodes.go
+++ common/exception/exceptionCodes.go
@@ -4,10 +4,13 @@ var (
 	TestIntendedException = NewException(9000, "测试错误")
 	UsrNotLogin           = NewException(1001, "用户未登录")
 	UsrNotPermitted       = NewException(1002, "用户无此权限")
+	UsrNotExisted         = NewException(1003, "用户不存在")
+	UsrPasswordErr       = NewException(1004, "用户密码错误")
 
 	ApiNoFormFile       = NewException(4001, "无文件字段")
 	ApiFileTooLarge     = NewException(4002, "上传文件过大")
 	ApiFileNotSupported = NewException(4003, "拒绝上传此类型文件类型")
+	ApiParamError       = NewException(4004, "参数错误")
 
 	SysUknExc              = NewException(5000, "未知错误")
 	SysCannotLoadPermGroup = NewException(5001, "内部异常: 无法从数据库读取权限表")
