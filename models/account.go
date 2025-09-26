d441f5bd9572a566a02106b044f3221f9518c20d
tonyp8 <tonyp8@126.com>
2025-09-26 21:29:34 +0800

    chore:改注释

--- models/account.go
+++ models/account.go
@@ -16,14 +16,15 @@ type AccountInfo struct {
 	UserName     string `json:"username" gorm:"column:username;index"`       // 用户显示名称
 	PermGroupID  uint32 `json:"permGroupID" gorm:"column:permGroupID;index"` // 用户所在权限组
 	AvatarFile   string `json:"avatarFile"`                                  //头像文件名
+	Activation   string `json:"-"`                                           //账户激活状态(保留,用于验证邮箱是否存在)
 
 	// 用户关联信息
 
 	RealName    string       `json:"realname" gorm:"column:realname;index"`
-	StudentID   string       `json:"studentID" gorm:"index"` //学号
+	StudentID   string       `json:"studentID" gorm:"index"` //学号/人员编号
 	Major       string       `json:"major"`                  //专业
-	Department  string       `json:"department"`             //院系
-	Grade       string       `json:"grade"`                  //年级
+	Department  string       `json:"department"`             //部门/院系 学生和管理员均有此项
+	Grade       string       `json:"grade"`                  //年级 F:2025
 	PhoneNumber string       `json:"phoneNumber"`            //手机号
 	CreatedAt   time.Time    `json:"createdAt" gorm:"index"`
 	UpdatedAt   time.Time    `json:"updatedAt"`
@@ -32,4 +33,5 @@ type AccountInfo struct {
 	// 备用
 
 	TwoFactorAuth string `json:"twoFactorAuth"` //双因素认证密钥  F:7J64V3P3E77J3LKNUGSZ5QANTLRLTKVL
+	WechatOpenID  string `json:"wechatOpenID"`  //微信openid，留给第三方做的，可以不是微信
 }
