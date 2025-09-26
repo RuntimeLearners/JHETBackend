9ca89171763936f2f6dba2d3de1a1f8aad5405ae
MucheXD <jmydaniel@outlook.com>
2025-09-26 21:36:16 +0800

    test: add_user test for create new pg

--- dao/account.go
+++ dao/account.go
@@ -1,5 +1,12 @@
 package dao
 
-func UpdateAccountAvatar(accountID uint64, fileName string) {
+import (
+	"JHETBackend/configs/database"
+	"JHETBackend/models"
+)
 
+func UpdateAccountAvatar(accountID uint64, fileName string) {
+	database.DataBase.Model(&models.AccountInfo{}).
+		Where("id = ?", accountID).
+		Update("avatar_file", fileName)
 }
