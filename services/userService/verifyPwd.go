bc4b7559974fbb07469b4d8e11ca14ab830a0e4d
tonyp8 <tonyp8@126.com>
2025-09-26 23:23:34 +0800

    fix:应XD的要求修改加密算法

--- services/userService/verifyPwd.go
+++ services/userService/verifyPwd.go
@@ -3,16 +3,22 @@ package userService
 import (
 	"JHETBackend/common/exception"
 	"JHETBackend/models"
-	"errors"
-
-	"golang.org/x/crypto/bcrypt"
+	"crypto/sha256"
+	"fmt"
 )
 
-// Verify Password
+// Verify Password using SHA256
 func VerifyPwd(user *models.AccountInfo, password string) error {
-	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
-	if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+	// 计算输入密码的 SHA256 哈希值
+	hash := sha256.Sum256([]byte(password))
+
+	// 将哈希值转换为十六进制字符串
+	hashedPassword := fmt.Sprintf("%x", hash)
+
+	// 比较存储的密码哈希和计算的哈希值
+	if user.PasswordHash != hashedPassword {
 		return exception.UsrPasswordErr
 	}
-	return err
+
+	return nil
 }
